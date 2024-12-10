9f5e09b8b87e12731cc748059a1c26768de447b0
Edward Lim <ed.lim199@gmail.com>
2024-12-10 18:38:04 +1100

    DRY the actions for login and signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Link }  from "./ui/NavBar"
 import NavBar from "./ui/NavBar"
 
 export default function Home() {
-  const links: Link[]= [{name: "Pricing", link: "/pricing"}, {name: "About Us", link: "/about-us"}, {name: "Login", link: "/login"}]
+  const links: Link[]= [{name: "Pricing", link: "/pricing"}, {name: "About Us", link: "/about-us"}, {name: "Login", link: "/login"}, {name: "Sign Up", link: "/signup"}]
   return (
     <>
       <NavBar links={links}/>
